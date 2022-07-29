f2c6e4c67e9a5faa46f82d37df458e37fa749589
cryptichic <syberchief@gmail.com>
2022-07-28 19:55:12 -0500

    added the contructor, struct ticker, and event tickerUpdate functionalities

--- FirstDapp-Starter/smartcontract/contracts/MarketSentiment.sol
+++ FirstDapp-Starter/smartcontract/contracts/MarketSentiment.sol
@@ -4,5 +4,25 @@ pragma solidity ^0.8.7;
 contract MarketSentiment {
     address public owner;
     string[] public tickersArray;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    struct ticker {
+        bool exists;
+        uint256 up;
+        uint256 down;
+        mapping(address => bool) Voters;
+    }
+
+    event tickerUpdate (
+        uint256 up,
+        uint256 down,
+        address voter,
+        string ticker
+    );
+    
 }
+
 
